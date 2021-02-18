a3796b1f4163448531bae4b71245a5740e7ed7b8
AliusDieMorietur <ikamqwerty@gmail.com>
2021-02-18 16:35:38 +0200

    tests added

---
+++ tests/test.js
@@ -0,0 +1,27 @@
+const tester = require('./tester');
+
+const { TemporaryStorage } = require('../target/lib/storage');
+
+(async () => {
+  await tester.test('TemporaryStorage.saveBuffer', {
+    fn: new TemporaryStorage().saveBuffer,
+    context: new TemporaryStorage('../target/storage', 600, { send(buffer) { return buffer; } }),
+    fnArgs: [Buffer.from([1, 2, 3])],
+    entityInspector: (context, result) => {
+      // console.log(context, result);
+      if (!(context.buffers[0] instanceof Buffer)) return 'Missed buffer';
+    }
+  })
+  
+  await tester.test('TemporaryStorage.download', {
+    fn: new TemporaryStorage().download,
+    context: new TemporaryStorage('./tests/test_storage', 600, { send(buffer) { return buffer; } }),
+    fnArgs: [{ token: 'test', files: ['Screenshot_2 (1).png'] }],
+    entityInspector: (context, result) => {
+      // console.log(context, result);
+      if (!result) return 'Missed result';
+    }
+  })
+  
+  tester.analysis();
+})();
---
+++ tests/tester.js
@@ -0,0 +1,81 @@
+const fs = require('fs');
+const { format } = require('util');
+
+const TEXTCOLORS = {
+  info: '\u001b[37m',
+  error: '\u001b[31m',
+  success: '\u001b[32m',
+};
+
+class Logger {
+  constructor() {
+    this.stream = fs.createWriteStream('./tests/log.txt', { flags: 'a' })
+  }
+
+  write(level, ...args) {
+    const s = format('', ...args);
+    const color = TEXTCOLORS[level];
+    const line = `${color}${s}`;
+    console.log(line + '\x1b[0m');
+    this.stream.write(`${s}` + '\n');
+  }
+
+  
+  log(...args) {
+    this.write('info', ...args);
+  }
+
+  error(...args) {
+    this.write('error', ...args);
+  }
+
+  success(...args) {
+    this.write('success', ...args);
+  }
+}
+
+class Tester {
+  constructor() {
+    this.failedTestscounter = 0;
+    this.passedTestscounter = 0;
+    this.logger = new Logger();
+  }
+
+  test = async (testName, testable) => {
+    const { 
+      fn, 
+      fnArgs, 
+      context,
+      entityInspector 
+    } = testable;   
+
+    try {
+      const fnWithContext = fn.bind(context);
+      const asyncified = fn instanceof (async () => {}).constructor
+        ? fnWithContext
+        : async (...args) => fnWithContext(...args)
+      const result = await asyncified(...fnArgs);
+      const error = entityInspector(context, result);
+      if (error) {
+        this.failedTestscounter++;
+        this.logger.error(`✘ Test failed on: ${testName} Error: ${error}`)
+      } else {
+        this.passedTestscounter++;
+        this.logger.success(`✔ Test passed on: ${testName} `);
+      };
+    } catch (error) {
+      this.logger.error(`✘ Test failed on: ${testName} Error: ${error}`);
+    }
+  }
+
+  analysis() { 
+    this.logger.log(
+      `\n` + 
+      ` Tests passed: ${this.passedTestscounter}\n` + 
+      ` Tests failed: ${this.failedTestscounter}\n` +
+      ` Generally: ${this.failedTestscounter + this.passedTestscounter}`
+    ) 
+  }
+}
+
+module.exports = new Tester();
