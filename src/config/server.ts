78e7eaeb7b698c9510fb30d438c43360709f2f29
AliusDieMorietur <ikamqwerty@gmail.com>
2021-03-01 19:50:07 +0200

    server config updated

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,5 +1,6 @@
 export const serverConfig = {
   ports: [7000, 7001, 7002, 7003],
   tokenLifeTime: 600000,
-  workers_quantity: 100
+  tmpStoragePath: './tmp_storage/'  ,
+  storagePath: './storage/'
 };
