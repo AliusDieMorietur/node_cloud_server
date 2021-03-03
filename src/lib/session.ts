c73c95621995e32fc2450163157f454d53a883fc
AliusDieMorietur <ikamqwerty@gmail.com>
2021-03-03 15:35:58 +0200

    contracts updated

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -12,7 +12,7 @@ export class Session {
   async createUser({ login, password }, ip: string): Promise<void> {
     const userToken = generateToken();
     await this.db.insert('SystemUser', { token: userToken, login, password });
-    const user = await this.getUser(login);
+    const user = await this.getUser('login', login);
     console.log(user);
     await this.db.insert('Session', { userid: user.id, ip, token: generateToken() });
     await fsp.mkdir(path.join(STORAGE_PATH, userToken));
@@ -23,22 +23,21 @@ export class Session {
   }
 
   async authUser({ login, password }, ip: string): Promise<string> {
-    const user = await this.getUser(login);
+    const user = await this.getUser('login', login);
     const token = password === password
       ? generateToken()
       : '';
     if (token.length === 0) throw new Error(`Wrong password`);
     const userId = user.id;
-    await this.deleteSessions(ip);
     await this.createSession({ userId, token, ip });
     return token;
   }
 
-  async getUser(login: string): Promise<any> {
-    const query = await this.db.select('SystemUser', ['*'], `login = '${login}'`);
+  async getUser(field: string, data: string): Promise<any> {
+    const query = await this.db.select('SystemUser', ['*'], `${field} = '${data}'`);
     const userExists = !!query.rows[0];
     console.log('userExists: ', userExists);
-    if (!userExists) throw new Error(`User with name <${login}> doesn't exist`);
+    if (!userExists) throw new Error(`User with ${field} <${data}> doesn't exist`);
     return query.rows[0];
   }
 
@@ -48,10 +47,6 @@ export class Session {
 
   async deleteSession(token: string): Promise<void> {
     this.db.delete('Session', `token = '${token}'`);
-  }
-
-  async deleteSessions(ip: string): Promise<void> {
-    this.db.delete('Session', `ip = '${ip}'`);
   }
 
   async restoreSession(token: string): Promise<any> {
