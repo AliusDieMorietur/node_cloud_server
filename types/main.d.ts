09e3aa21238c127759af8152515ceb604f848824
AliusDieMorietur <ikamqwerty@gmail.com>
2021-03-08 17:00:31 +0200

    command types added

---
+++ types/main.d.ts
@@ -0,0 +1,21 @@
+type Structure = {
+  name: string, 
+  childs?: Structure[],
+  capacity: number
+} 
+
+type CommandArgs = {
+  token?: string,
+  storageName? : string,
+  fileList?: string[],
+  currentPath?: string, 
+  filePath?: string,
+  changes?: [ [string, string] ], 
+  user?: { login: string, password: string }
+}
+
+type CommandResults = {
+  token?: string,
+  fileList?: string[],
+  structure?: Structure[]
+}
