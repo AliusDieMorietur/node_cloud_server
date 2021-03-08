714e77327aef5f171b3f9f3cfb710e3984459ff2
AliusDieMorietur <ikamqwerty@gmail.com>
2021-03-08 17:03:14 +0200

    links support and structure updates distribution added

--- src/lib/app.ts
+++ src/lib/app.ts
@@ -1,8 +1,13 @@
 import { promises as fsp, Dirent } from 'fs';
 import * as path from 'path';
 import { Logger } from './logger';
+import Database from './db';
+import { dbConfig } from '../config/db';
+import { serverConfig } from '../config/server'; 
+import { generateToken } from './auth';
 
 const STATIC_PATH = path.join(process.cwd(), './static');
+const STORAGE_PATH: string = path.join(process.cwd(), serverConfig .storagePath);
 
 const toUnix = (filePath: string): string => 
   process.platform === 'win32' 
@@ -12,11 +17,51 @@ const toUnix = (filePath: string): string =>
     : filePath;
 
 export class App {
+  private static = new Map<string, Buffer>();
+  private links = new Map<string, string>();
+  private connections = new Map<string, any>();
   logger = new Logger();
-  static = new Map<string, Buffer>();
+  db: Database;
+
+  saveConnection(login: string, connection): number {
+    const connections = this.connections.has(login)
+      ? this.connections.get(login)
+      : this.connections.set(login, []).get(login);
+    console.log(connections);
+    return connections.push(connection);
+  }
+  
+  deleteConnection(login: string, index: number) {
+    this.connections.get(login)[index] = null;
+  }
 
   getStatic(filePath: string): Buffer {
     return this.static.get(filePath);
+  }
+  
+  async getInfo(token: string): Promise<{ [key: string]: string }> {
+    const info = await fsp.readFile(path.join(STORAGE_PATH, token + '_info.json'));
+    return JSON.parse(info.toString()).savedNames;
+  }
+
+  async createLink(filePath: string, userToken: string): Promise<string> {
+    const fileName = filePath.split('/').filter((e, i, a) => i === a.length - 1);
+    const list = await this.getInfo(userToken);
+    const token = generateToken();
+    const link = `${userToken}:${list[filePath]}:${fileName}`;
+    this.links.set(token, link);
+    await this.db.insert('Link', { token, link });
+    return token;
+  }
+
+  getLink(token: string): string {
+    return this.links.get(token);
+  } 
+
+  async loadLinks(): Promise<void> { 
+    const links = await this.db.select('Link');
+    for (const row of links.rows) 
+      this.links.set(row.token, row.link);
   }
 
   async loadFile(filePath: string, storage: Map<String, Buffer>) {
@@ -46,7 +91,15 @@ export class App {
   }
 
   async start() {
-    await this.loadDirectory(STATIC_PATH, this.static);
-    this.logger.success('Static loaded');
+    try {
+      this.db = new Database(dbConfig);
+      this.logger.success('Db connected');
+      await this.loadLinks();
+      this.logger.success('Links loaded');
+      await this.loadDirectory(STATIC_PATH, this.static);
+      this.logger.success('Static loaded');
+    } catch (error) {
+      this.logger.error(error);
+    }
   }
 }
