6e9ecb31addd38c02da91976914aee549853e17d
AliusDieMorietur <ikamqwerty@gmail.com>
2021-03-16 01:54:40 +0200

    host and maxpayload parametrs added

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,6 +1,8 @@
 export const serverConfig = {
-  ports: [80],
+  ports: [7000],
+  host: '192.168.0.136',
   tokenLifeTime: 600000,
+  maxPayload: 1000000000,
   tmpStoragePath: './tmp_storage/',
   storagePath: './storage/'
 };
