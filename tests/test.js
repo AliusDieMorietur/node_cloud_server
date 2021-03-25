4da0054c44388fec8b1afb4cb3e5adf546a01b0c
AliusDieMorietur <ikamqwerty@gmail.com>
2021-03-25 20:20:16 +0200

    tests for validate functions added; additional tests added

--- tests/test.js
+++ tests/test.js
@@ -5,6 +5,16 @@ const { Storage } = require('../target/lib/storage');
 
 tester.start(async test => [
 
+  test('asyncFunctionExample', {
+    context: null,
+    fn: async () => new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(1);
+      }, Math.floor(Math.random() * 500));
+    }),
+    fnArgs: [[]]
+  }),
+
   test('Storage.buildStructure', {
     context: Storage,
     fn: Storage.buildStructure,
@@ -67,6 +77,17 @@ tester.start(async test => [
         ]
       ],
       [
+        [
+          {
+            id: 162,
+            token: 'q1gw0osQxqdfiiOdfoYRrD5zK2gz1Oas',
+            name: 'New Folder/',
+            fakename: 'folder',
+            size: 0
+          }
+        ]
+      ],
+      [
         []
       ]
     ],
@@ -92,8 +113,17 @@ tester.start(async test => [
           capacity: 11141537
         }
       ],
+      [
+        {
+          name: 'New Folder',
+          childs: [],
+          capacity: 0
+        }
+      ],
       []
-    ]
+    ],
+    specialRules: (context, result) => {
+    }
   }),
 
   test('Storage.findPlace', {
@@ -154,21 +184,127 @@ tester.start(async test => [
         [1, 4],
         [2, 5],
         [3, 6]
+      ],
+      [
+        [1, 4],
+        [2, 5],
+        [3, null]
+      ],
+      [
+        [1, 4],
+        [2, 5],
+        [null, 6]
+      ],
+      [],
+      [
+        [1, null],
+        [2, null],
+        [3, null]
       ]
     ]
-  })
+  }),
 
-  // test('validate', {
-  //   context: null,
-  //   fn: validate,
-  //   fnArgs: [
-  //     [
-  //       'login',
-  //       'AZaz09'
-  //     ]
-  //   ],
-  //   specialRules: (context, result) => {
-  //     // console.log(result);
-  //   }
-  // })
+  test('validate.login', {
+    context: null,
+    fn: validate.login,
+    fnArgs: [
+      ['Ab0oihf_.'],
+      ['Ab0_.oihfasudhbfuasasdfojnasfjuasas'],
+      ['weq'],
+      ['']
+    ],
+    expectedResults: [
+      true,
+      false,
+      false,
+      false
+    ],
+    specialRules: (context, result, args) => {}
+  }),
+
+  test('validate.password', {
+    context: null,
+    fn: validate.password,
+    fnArgs: [
+      ['normalPassword'],
+      ['normal Password'],
+      ['6ELwoCSq9UPvo246Rp2b8JO7KSNCrAmx'],
+      ['weq'],
+      ['']
+    ],
+    expectedResults: [
+      true,
+      false,
+      false,
+      false,
+      false
+    ],
+    specialRules: (context, result, args) => {}
+  }),
+
+  
+  test('validate.token', {
+    context: null,
+    fn: validate.token,
+    fnArgs: [
+      ['6ELwoCSq9UPvo246Rp2b8JO7KSNCrAmx'],
+      ['mFtnBJIIba8H1HEmGfucVshUIjKBZD2c'],
+      ['6ELwoCSq9UPv$246Rp/b8J:7KSNCrAmx'],
+      ['6ELwoCSq9UPvo246Rp2b8JO7KSNCrAmxasds'],
+      ['weq'],
+
+    ],
+    expectedResults: [
+      true,
+      true,
+      false,
+      false,
+      false
+    ],
+    specialRules: (context, result, args) => {}
+  }),
+  
+  test('validate.name', {
+    context: null,
+    fn: validate.name,
+    fnArgs: [
+      ['6ELwoCSq9UPvo246Rp2b8JO7KSNCrAmx'],
+      ['Folder or File name'],
+      ['File (1).js'],
+      ['1-kek.ts'],
+      ['1_kek.rs'],
+      ['Folder/1_kek.rs'],
+      ['folder/folder/Folder/1_kek.rs'],
+      ['6ELwoCSq9UPvo246Rp2b8JO7KSNCrAmx6ELwoCSq9UPvo246Rp2b8JO7KSNCrA'],
+      ['folder/folder//file'],
+      ['folder//folder/'],
+      ['folder////folder/'],
+      ['folder///////folder/'],
+      ['/'],
+      ['/folder/folder1/'],
+      ['/folder/file'],
+      [''],
+      [':/wq#@$']
+    ],
+    expectedResults: [
+      true,
+      true,
+      true,
+      true,
+      true,
+      true,
+      true,
+      true,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false
+    ],
+    specialRules: (context, result, args) => {}
+  }),
 ]);
