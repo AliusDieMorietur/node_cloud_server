d33f2ec67f43ea258d5c44541be1c623ed3786f6
AliusDieMorietur <ikamqwerty@gmail.com>
2021-03-25 20:21:51 +0200

    validate functions implementation added

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,38 +1,45 @@
-export const zip = (arr1, arr2) => {
-  const [longest, shortest] = arr1.length < arr2.length
-  ? [arr2, arr1]
-  : [arr1, arr2];
+// export const zip = (arr1, arr2) => {
+//   const [longest, shortest] = arr1.length < arr2.length
+//   ? [arr2, arr1]
+//   : [arr1, arr2];
   
-  return longest.map((item, index) => [
-    item, 
-    shortest[index] ? shortest[index] : null  
-  ]);
-}
+//   return longest.map((item, index) => [
+//     item, 
+//     shortest[index] ? shortest[index] : null  
+//   ]);
+// }
+
+export const zip = (arr1, arr2) => 
+  (arr1.length < arr2.length ? arr2 : arr1)
+    .map((item, index) => [
+      arr1[index] !== undefined ? arr1[index] : null, 
+      arr2[index] !== undefined ? arr2[index] : null  
+    ]);
 
 const TOKEN_LENGTH = 32;
-const LOGIN_PASSWORD_LENGTH = 16;
-const ALPHA_UPPER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const ALPHA_LOWER = 'abcdefghijklmnopqrstuvwxyz';
-const ALPHA = ALPHA_UPPER + ALPHA_LOWER;
-const DIGIT = '0123456789';
-const ALPHA_DIGIT = ALPHA + DIGIT;
+const LOGIN_PASSWORD_MIN_LENGTH = 5;
+const LOGIN_PASSWORD_MAX_LENGTH = 16;
+const NAME_MIN_LENGTH = 1;
+const NAME_MAX_LENGTH = 1024;
 
-
-const inputs = {
-  login: str => {
-    return  new RegExp('/\W/').test(str);
-  },
-  password: str => {
-
-  },
-  token: str => {
-
-  },
-  name: str => {
-
-  }
+export const validate = {
+  login: str => 
+    new RegExp('^[A-Za-z0-9_.]+$').test(str) &&
+    str.length <= LOGIN_PASSWORD_MAX_LENGTH &&
+    str.length >= LOGIN_PASSWORD_MIN_LENGTH,
+  password: str =>     
+    new RegExp('^[A-Za-z0-9]+$').test(str) &&
+    str.length <= LOGIN_PASSWORD_MAX_LENGTH &&
+    str.length >= LOGIN_PASSWORD_MIN_LENGTH,
+  token: str =>     
+    new RegExp('^[A-Za-z0-9]+$').test(str) &&
+    str.length === TOKEN_LENGTH,
+  name: str => 
+    new RegExp('^[A-Za-z0-9\-/_.() ]+$').test(str) &&
+    str[0] !== '/' &&
+    !str.includes('//') &&
+    str.length <= NAME_MAX_LENGTH &&
+    str.length >= NAME_MIN_LENGTH,
 };
 
-export const validate = (input, str) => {
-  return inputs[input](str);
-};
+
