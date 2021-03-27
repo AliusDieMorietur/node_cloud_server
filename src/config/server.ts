3da28ddf2de8905d400556d33e8c2936043ea194
AliusDieMorietur <ikamqwerty@gmail.com>
2021-03-27 14:04:15 +0200

    revert to default

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,7 +1,7 @@
 export const serverConfig = {
   ports: [7000],
-  host: '192.168.0.136',
-  tokenLifeTime: 10000,
+  host: 'localhost',
+  tokenLifeTime: 600000,
   maxPayload: 1000000000,
   storagePath: './storage/'
 };
