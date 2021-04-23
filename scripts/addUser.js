b36a64b58ff2ff4c7d442fe77a9e707d9f10c0a2
AliusDieMorietur <ikamqwerty@gmail.com>
2021-04-23 12:58:17 +0300

    script updated

--- scripts/addUser.js
+++ scripts/addUser.js
@@ -5,65 +5,40 @@ const fsp = fs.promises;
 const path = require('path');
 const pg = require('pg');
 const { Pool } = pg;
-const crypto = require('crypto');
-
-const BYTE = 256;
-const TOKEN_LENGTH = 32;
-const ALPHA_UPPER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const ALPHA_LOWER = 'abcdefghijklmnopqrstuvwxyz';
-const ALPHA = ALPHA_UPPER + ALPHA_LOWER;
-const DIGIT = '0123456789';
-const ALPHA_DIGIT = ALPHA + DIGIT;
-
-const generateToken = () => {
-  const base = ALPHA_DIGIT.length;
-  const bytes = crypto.randomBytes(base);
-  let key = '';
-  for (let i = 0; i < TOKEN_LENGTH; i++) {
-    const index = ((bytes[i] * base) / BYTE) | 0;
-    key += ALPHA_DIGIT[index];
-  }
-  return key;
-};
+const dbConfig = require('../config/db');
+const { generateToken } = require('../lib/utils');
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
+const ask = (str) => new Promise((resolve, reject) => {
+  rl.question(str, (input) => {
+    resolve(input);
+  });
+});
+
 (async () => {
   try {
-    const pool = new Pool({
-      user: 'admin',
-      host: 'localhost',
-      database: 'cloud_storage',
-      password: 'admin',
-      port: 5432,
-    });
-
-    let login = '';
-    let password = '';
-    rl.question('Login: ', loginInput => {
-      rl.question('Password:  ', async passwordInput => {
-        if (loginInput.length < 5 && passwordInput.length < 5) {
-          throw new Error('Too short password or login');
-        }
-        login = loginInput;
-        password = passwordInput;
-        const userToken = generateToken();
-        await pool.query(
-          `INSERT INTO SystemUser(Token, Login, Password) ` +
-            `VALUES ('${userToken}', '${login}', '${password}')`
-        );
-        console.log(`User <${login}> created`);
-        await pool.query(
-          `INSERT INTO  StorageInfo(Token, Expire) VALUES ('${userToken}', 0)`
-        );
-        await fsp.mkdir(path.join('./storage', userToken));
-        console.log(`Storage <${userToken}> for <${login}> created`);
-        rl.close();
-      });
-    });
+    const pool = new Pool(dbConfig);
+    let login = process.argv[2] ? process.argv[2] : await ask('Login: ');
+    let password = process.argv[3] ? process.argv[3] : await ask('Password: ');
+    rl.close();
+    if (login.length < 5 && password.length < 5) {
+      throw new Error('Too short password or login');
+    }
+    const userToken = generateToken();
+    await pool.query(
+      `INSERT INTO SystemUser(Token, Login, Password) ` +
+      `VALUES ('${userToken}', '${login}', '${password}')`
+    );
+    console.log(`User <${login}> created`);
+    await pool.query(
+      `INSERT INTO  StorageInfo(Token, Expire) VALUES ('${userToken}', 0)`
+    );
+    await fsp.mkdir(path.join('./storage', userToken));
+    console.log(`Storage <${userToken}> for <${login}> created`);
   } catch (error) {
     console.error(error);
   }
