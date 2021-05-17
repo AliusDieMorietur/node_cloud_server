ef063b5f9a9f3b94445220be0852e464292900d7
gidra5 <hranitel.52@gmail.com>
2021-05-17 13:49:02 +0300

    restructure docker specific files

---
+++ config/docker/db.js
@@ -0,0 +1,8 @@
+'use strict';
+module.exports = {
+  user: 'admin',
+  host: 'db',
+  database: 'cloud_storage',
+  password: 'admin',
+  port: 5432,
+};
---
+++ config/docker/server.js
@@ -0,0 +1,7 @@
+'use strict';
+module.exports = {
+  ports: [7000],
+  host: 'localhost', //'192.168.0.136',
+  tokenLifeTime: 10000,
+  maxPayload: 1000000000,
+};
